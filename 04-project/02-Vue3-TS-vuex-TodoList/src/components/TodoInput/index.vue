c5beaa249837aa0179cb81192b5998649e699e22
nice <nice@google.com>
2023-02-25 00:01:45 +0800

    TodoList-Vue3-TS-Vuex: 	1. 类型文件定义 	2. input 组件完善:

--- 04-project/02-Vue3-TS-vuex-TodoList/src/components/TodoInput/index.vue
+++ 04-project/02-Vue3-TS-vuex-TodoList/src/components/TodoInput/index.vue
@@ -5,8 +5,9 @@
 
 <script lang="ts" setup>
 import { ref } from 'vue';
-const todo = ref<string>('');
+const todo = ref<string>(''); //ref 数据，指定 TS 类型
 
+// input 框，键盘抬起事件：添加 todo 数据、todo 赋值为空
 const addTodo = (e: KeyboardEvent): void => {
   if (e.key === 'Enter' && todo.value.trim().length) {
     console.log(todo.value);
--- 04-project/02-Vue3-TS-vuex-TodoList/src/store/state.ts
+++ 04-project/02-Vue3-TS-vuex-TodoList/src/store/state.ts
@@ -1,1 +1,18 @@
 export default {};
+
+/*
+状态 以及 状态类型划分
+
+list: ITodo[]
+
+
+ITodo
+listItem:{
+    id: new Data().getTime() => number
+    content: string
+    status:FINISHED  DOING WILLDO => 枚举类型
+}
+
+**** 为什么推荐使用 interface ，而不是 type？
+        interface 可以使用 extends，复用性强
+*/
